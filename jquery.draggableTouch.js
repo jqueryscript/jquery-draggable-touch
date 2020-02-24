8f639e2460c57da53fa8bdf426f00d1758fc4d80
yuqianyumo <iqq800@gmail.com>
2020-02-24 10:09:18 +0800

    Replace bind() & unbind() with on() & off()

--- jquery.draggableTouch.js
+++ jquery.draggableTouch.js
@@ -1,14 +1,14 @@
 /**
  * jQuery Draggable Touch v0.5
- * Jonatan Heyman | http://heyman.info 
+ * Jonatan Heyman | http://heyman.info
  *
  * Make HTML elements draggable by using uses touch events.
- * The plugin also has a fallback that uses mouse events, 
+ * The plugin also has a fallback that uses mouse events,
  * in case the device doesn't support touch events.
- * 
+ *
  * Licenced under THE BEER-WARE LICENSE (Revision 42):
- * Jonatan Heyman (http://heyman.info) wrote this file. As long as you retain this 
- * notice you can do whatever you want with this stuff. If we meet some day, and 
+ * Jonatan Heyman (http://heyman.info) wrote this file. As long as you retain this
+ * notice you can do whatever you want with this stuff. If we meet some day, and
  * you think this stuff is worth it, you can buy me a beer in return.
  */
 ;(function($){
@@ -18,31 +18,31 @@
         if (!("ontouchstart" in document.documentElement)) {
             return this.draggableMouse(action);
         }
-        
+
         // check if we shall make it not draggable
         if (action == "disable") {
-            this.unbind("touchstart");
-            this.unbind("touchmove");
-            this.unbind("touchend");
-            this.unbind("touchcancel");
+            this.off("touchstart");
+            this.off("touchmove");
+            this.off("touchend");
+            this.off("touchcancel");
             return this;
         }
-        
+
         this.each(function() {
             var element = $(this);
             var offset = null;
-            
+
             var end = function(e) {
                 e.preventDefault();
                 var orig = e.originalEvent;
-                
+
                 element.trigger("dragend", {
                     top: orig.changedTouches[0].pageY - offset.y,
                     left: orig.changedTouches[0].pageX - offset.x
                 });
             };
-            
-            element.bind("touchstart", function(e) {
+
+            element.on("touchstart", function(e) {
                 var orig = e.originalEvent;
                 var pos = $(this).position();
                 offset = {
@@ -51,40 +51,40 @@
                 };
                 element.trigger("dragstart", pos);
             });
-            element.bind("touchmove", function(e) {
+            element.on("touchmove", function(e) {
                 e.preventDefault();
                 var orig = e.originalEvent;
-                
+
                 // do now allow two touch points to drag the same element
                 if (orig.targetTouches.length > 1)
                     return;
-                
+
                 $(this).css({
                     top: orig.changedTouches[0].pageY - offset.y,
                     left: orig.changedTouches[0].pageX - offset.x
                 });
             });
-            element.bind("touchend", end);
-            element.bind("touchcancel", end);
+            element.on("touchend", end);
+            element.on("touchcancel", end);
         });
         return this;
     };
-    
+
     /**
      * Draggable fallback for when touch is not available
      */
     $.fn.draggableMouse = function (action) {
         // check if we shall make it not draggable
         if (action == "disable") {
-            this.unbind("mousedown");
-            this.unbind("mouseup");
+            this.off("mousedown");
+            this.off("mouseup");
             return this;
         }
-        
+
         this.each(function() {
             var element = $(this);
             var offset = null;
-            
+
             var move = function(e) {
                 element.css({
                     top: e.pageY - offset.y,
@@ -92,21 +92,21 @@
                 });
             };
             var up = function(e) {
-                element.unbind("mouseup", up);
-                $(document).unbind("mousemove", move);
+                element.off("mouseup", up);
+                $(document).off("mousemove", move);
                 element.trigger("dragend", {
                     top: e.pageY - offset.y,
                     left: e.pageX - offset.x
                 });
             };
-            element.bind("mousedown", function(e) {
+            element.on("mousedown", function(e) {
                 var pos = element.position();
                 offset = {
                     x: e.pageX - pos.left,
                     y: e.pageY - pos.top
                 };
-                $(document).bind("mousemove", move);
-                element.bind("mouseup", up);
+                $(document).on("mousemove", move);
+                element.on("mouseup", up);
                 element.trigger("dragstart", pos);
                 e.preventDefault();
             });
